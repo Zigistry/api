c05cc68f5d2ce86bd4efcaec55da42aad4db659b
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-05-02 00:39:10 +0530

    Was able to find the page_needed and the offset correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 // doing #include <crow.h> works,
 // but added this to maintain cross-platformity
 #include "../include/crow_all.h"
+#include <algorithm>
 #include <climits>
+#include <cmath>
 #include <string>
 
 libsql_connection_t database_connection;
@@ -57,7 +59,14 @@ crow::response search_packages(const crow::request& req)
     const unsigned int start_index = page * per_page;
     const unsigned int end_index = start_index + per_page;
 
-    const std::string search_packages_count_query = R"""(
+    // this will convert something like:
+    // hello
+    // into:
+    // '%hello%'
+
+    const std::string query = std::format("'%{}%'", search_query);
+
+    std::string search_packages_count_query = std::format(R"""(
 
 
             SELECT COUNT(*) FROM repos
@@ -71,23 +80,55 @@ crow::response search_packages(const crow::request& req)
                 EXISTS (
                     SELECT 1 FROM repo_search
                     WHERE repo_search.repo_id = repos.id
-                    AND keywords MATCH ?
+                    AND keywords MATCH {}
                 )
 
-                OR repos.id LIKE ?
-                OR repos.owner LIKE ?
-                OR repos.description LIKE ?
-                OR repos.primary_language LIKE ?
+                OR repos.id LIKE {}
+                OR repos.owner LIKE {}
+                OR repos.description LIKE {}
+                OR repos.primary_language LIKE {}
             );
 
 
-    )""";
+    )""",
+        query, query, query, query, query);
 
     const libsql_statement_t count_stmt = libsql_connection_prepare(
-        database_connection, search_packages_count_query.c_str()
-    );
+        database_connection, search_packages_count_query.c_str());
+
+    unsigned int total_results = 0;
+
+    if (not count_stmt.err) {
+        const libsql_rows_t count_rows = libsql_statement_query(count_stmt);
+        const libsql_row_t count_row = libsql_rows_next(count_rows);
+        if (not count_row.err and not libsql_row_empty(count_row)) {
+            total_results = (unsigned int)libsql_row_value(count_row, 0).ok.value.integer;
+        }
+    }
+
+    if (total_results == 0) {
+        crow::json::wvalue empty_responce;
+        return crow::response(empty_responce);
+    }
+
+    // here, we can get like, 75 / 10
+    // i.e 7.5, hence this would require 8 pages.
+    // i.e ceil(7.5) -> 8
+    const float pages_needed_in_decimal = (float)total_results / per_page;
 
     
+    const unsigned long total_pages = std::ceil(pages_needed_in_decimal);
+
+    // either the first page, or the requested one
+    // also, to make sure page is in range
+    const unsigned long page_needed = std::max(1ul, std::min(total_pages, page));
+
+    
+    const unsigned long offset = (page_needed - 1) * per_page;
+
+    
+
+
     crow::json::wvalue normal_responce;
     return crow::response(normal_responce);
 }
@@ -98,11 +139,11 @@ int main()
     const char* api_key = getenv("API_KEY");
 
     if (database_url == NULL or api_key == NULL) {
-        printf("Oh no! Problem! No env keys\n");
+        std::cout << "Oh no! Problem! No env keys" << std::endl;
         return 1;
     }
 
-    libsql_setup(libsql_config_t { });
+    libsql_setup(libsql_config_t {});
 
     const libsql_database_t db = libsql_database_init((libsql_database_desc_t) {
         .url = database_url,
@@ -114,15 +155,20 @@ int main()
     });
 
     if (db.err) {
-        fprintf(stderr, "Error: %s\n", libsql_error_message(db.err));
+        std::cerr << "Error:\n"
+                  << libsql_error_message(db.err) << std::endl;
         return 1;
     }
-    libsql_connection_t database_connection = libsql_database_connect(db);
-    printf("CONNECTED!!!!\n");
+
+    database_connection = libsql_database_connect(db);
+
+    std::cout << "Connected..." << std::endl;
+
     if (database_connection.err) {
-        fprintf(stderr, "Connection error: %s\n", libsql_error_message(database_connection.err));
+        std::cerr << "Connection error: " << libsql_error_message(database_connection.err) << std::endl;
         return 1;
     }
+
     crow::SimpleApp app;
     CROW_ROUTE(app, "/search/packages/")(search_packages);
     app.port(3000).multithreaded().run();
